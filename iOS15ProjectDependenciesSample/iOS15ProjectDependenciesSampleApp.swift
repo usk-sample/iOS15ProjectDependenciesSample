679a4df22fe2656f653927ea8eb10901872df12c
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-09-16 10:16:49 +0900

    Initial Commit

---
+++ iOS15ProjectDependenciesSample/iOS15ProjectDependenciesSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15ProjectDependenciesSampleApp.swift
+//  iOS15ProjectDependenciesSample
+//
+//  Created by Yusuke Hasegawa on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct iOS15ProjectDependenciesSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
